3617dcb95b6ddbdb326df989f5ecaedd030e3ec2
kouji sugimoto <kouji36000@gmail.com>
2018-03-10 22:19:56 +0900

    init

---
+++ js/eventpage.js
@@ -0,0 +1,51 @@
+﻿var tweet;
+var wsUri = "ws://ec2-52-40-135-194.us-west-2.compute.amazonaws.com:1880/ws/twitter";
+var ws = new WebSocket(wsUri);
+
+/* websocketリスナ[ */
+ws.onmessage = function (evt) {
+
+    tweet = JSON.parse(evt.data);
+    var id = tweet.id;
+    var target = tweet.target;
+    
+    //localstorageに含まれていなければ通知しない
+    var storage = localStorage["help"];
+    if(storage.indexOf(target) == -1){
+        return;
+    }
+
+    var opt = {
+        type: 'basic',
+        title: target,
+        message: id,
+        iconUrl: 'images/'+target+'.jpg'
+    }
+    chrome.notifications.create(id, opt, function(id){});
+};
+
+
+chrome.notifications.onClicked.addListener(function(id){
+    console.log(id);
+    var temp = document.createElement('textarea');
+
+    temp.value = id;
+    temp.selectionStart = 0;
+    temp.selectionEnd = temp.value.length;
+
+    var s = temp.style;
+    s.position = 'fixed';
+    s.left = '-100%';
+
+    document.body.appendChild(temp);
+    temp.focus();
+    var result = document.execCommand('copy');
+    temp.blur();
+    document.body.removeChild(temp);
+    chrome.notifications.clear(id, function(id){});
+    //
+    // document.execCommand('copy');
+});
+
+
+
---
+++ js/popup.js
@@ -0,0 +1,35 @@
+﻿$(function() {
+ 
+  $('input[name="help[]"]').change(function() {
+ 
+    var helps = [];
+    $('input[name="help[]"]:checked').each(function() {
+      helps.push($(this).val());
+    });
+    
+    // ブラウザのストレージに保存
+    localStorage["help"] = helps;
+    
+    boxShow();
+  });
+});
+
+function boxShow(){
+    if(localStorage["help"]){
+        $('#checkedBox').text("選択中："+localStorage["help"])
+    }else{
+        $('#checkedBox').text("現在何も選択されていません。");
+    }
+}
+
+function boxCheck(){
+    var boxes = document.getElementsByName("help[]");
+    var cnt = boxes.length;
+    var storage = localStorage["help"];
+    
+    for(var i=0; i<cnt; i++) {
+        if(storage.indexOf(boxes.item(i).value) >= 0) {
+           boxes.item(i).checked = true;
+        }
+    }
+}
